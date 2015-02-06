aa828805a578ee2005596994f1d27da04eebfec9
Ben Smith <ben@z2.com>
2015-02-06 00:12:50 -0800

    Debug off

--- local_settings.py
+++ local_settings.py
@@ -1,4 +1,4 @@
-DEBUG = True #Set this to False to start Tweeting live
+DEBUG = False #Set this to False to start Tweeting live
 MAX_CHARACTERS = 140 #Max tweet length
 CAP_LENGTH = False #Should we cap the length of a tweet?
 TWEET_ACCOUNT = "golf_wisdom" #The name of the account you're tweeting to.
