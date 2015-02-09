98e0e120d3fe8134111a6defe648b0e4701ef330
Ben Smith <ben@z2.com>
2015-02-09 13:28:32 -0800

    Better chance of tweeting

--- local_settings.py
+++ local_settings.py
@@ -3,4 +3,4 @@ MAX_CHARACTERS = 140 #Max tweet length
 CAP_LENGTH = False #Should we cap the length of a tweet?
 TWEET_ACCOUNT = "golf_wisdom" #The name of the account you're tweeting to.
 MAX_TRIES = 100
-CHANCE = 0.2
+CHANCE = 0.25
